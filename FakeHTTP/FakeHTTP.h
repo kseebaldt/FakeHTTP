3e099f81fe7f8fb546325683cec7d7b8d9d405d9
Kurtis Seebaldt <kseebaldt@gmail.com>
2012-10-04 13:10:26 -0700

    Initial Commit

---
+++ FakeHTTP/FakeHTTP.h
@@ -0,0 +1,13 @@
+//
+//  FakeHTTP.h
+//  FakeHTTP
+//
+//  Created by Kurtis Seebaldt on 10/4/12.
+//  Copyright (c) 2012 Kurtis Seebaldt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FakeHTTP : NSObject
+
+@end
---
+++ FakeHTTP/FakeHTTP.m
@@ -0,0 +1,13 @@
+//
+//  FakeHTTP.m
+//  FakeHTTP
+//
+//  Created by Kurtis Seebaldt on 10/4/12.
+//  Copyright (c) 2012 Kurtis Seebaldt. All rights reserved.
+//
+
+#import "FakeHTTP.h"
+
+@implementation FakeHTTP
+
+@end
