49bf9c1664fd91e6d1fc26840fac224d1b5dd173
Brian Croom <bcroom@pivotal.io>
2015-10-26 21:51:41 -0400

    Update the Cedar iOS framework to 0.12.0

---
+++ Specs/main.mm
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Specs
+//
+//  Created by Kurtis Seebaldt on 10/4/12.
+//  Copyright (c) 2012 Kurtis Seebaldt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <Cedar/Cedar.h>
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"CedarApplicationDelegate");
+    }
+}
